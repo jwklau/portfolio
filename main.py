33769c53b7f1ce0c47e2badd5dc451c2b57ee4f2
jwklau <nuffah@gmail.com>
2023-02-15 17:45:54 +0000

    Add header and footer templates, update all internal links to dynamic URLs, add back in contact page

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__)
 
+current_year = datetime.now().year
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", year=current_year)
 
 @app.route("/python-projects")
 def load_py_projects():
-    return render_template("python-projects.html")
+    return render_template("python-projects.html", year=current_year)
 
 @app.route("/web-development-projects")
 def load_web_dev_projects():
-    return render_template("web-development-projects.html")
+    return render_template("web-development-projects.html", year=current_year)
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", year=current_year)
 
 if __name__ == "__main__":
     app.run(debug=True)
